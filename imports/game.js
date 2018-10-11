8a4e35b7a84d3d77317905164f9387abbeaee127
Mikkel Syse Groesland <mikkel@spinnerlabs.no>
2018-10-11 10:44:59 +0200

    added game and ui class files.

---
+++ imports/game.js
@@ -0,0 +1,27 @@
+export default class Game {
+
+	constructor(playType, maxValue, isHuman) {
+		this._playType = playType; //0 = max rounds, 1 = reach score
+		this._value = 0; //current value, either round or highest score
+		this._maxValue = maxValue; //value to reach, either rounds or score
+		this._isHuman = isHuman;
+	}
+
+	get playType() { return this._playType }
+	get value() { return this._value }
+	get maxValue() { return this._maxValue }
+	get isHuman() { return this._isHuman }
+
+	set playType(value) { this._playType = value }
+	set value(value) { this._value = value }
+	set maxValue(value) { this._maxValue = value }
+	set isHuman(value) { this._isHuman = value }
+
+	isGameOver() {
+		return (this._value >= this._maxValue);
+	}
+
+	incValue() {
+		this._value++;
+	}
+}
