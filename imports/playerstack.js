a2f65f34936a747c24a8824bbeaead47fb04bd30
Mikkel Syse Groesland <mikkelgro@gmail.com>
2018-12-26 14:02:06 +0100

    added PlayerStack class

---
+++ imports/playerstack.js
@@ -0,0 +1,48 @@
+'use strict';
+
+export default class PlayerStack {
+
+	constructor(players) {
+        this._players = players;
+        this._size = players.length;
+        this._pos = 0;
+    }
+
+    next() {
+        const player = this._player[this._pos++];
+
+        if (!this.hasNext()) {
+            this.nextDealer();
+            this._pos = 0;
+        } 
+
+        return player;
+    }
+
+    hasNext() {
+        return this._pos < this._size - 1;
+    }
+
+    current() {
+        return this._players[this._pos];
+    }
+
+    nextTo() {
+        if (this.hasNext()) {
+            return this._players[this._pos + 1];
+        } else {
+            return { name: 'deck', isDeck: true };
+        }
+    }
+
+    nextDealer() {
+        const oldDealer = this._players.pop(); //pop out current dealer player
+        this._players.unshift(oldDealer); //insert that player at start of array
+
+        console.log(`Dealer is now: ${this.dealer().name}`);
+    }
+
+    dealer() {
+        return this._players[this._size - 1];
+    }
+}
